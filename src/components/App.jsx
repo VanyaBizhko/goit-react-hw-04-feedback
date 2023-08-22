4681e10fde01a91f5686403fb33f65d4c1e1d2a4
VanyaBizhko <vanyabigko@gmail.com>
2023-08-22 17:42:01 +0300

    first try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,63 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Statistics from './Statistics/Statistics';
 import Message from './Message/Message';
 import Section from './Section/Section';
 import Feddback from './Feddback/Feddback';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  
+export default function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+ const handleChange = (e) => {
+ 
+   switch (e) {
+     case 'good':
+      setGood (good +1);
+       break
+     case 'neutral':
+       setNeutral (neutral + 1);
+       break
+     case 'bad':
+       setBad (bad + 1);
+       break
+     default:
+       return
+   }
+ }
+  const handleTotalFeddback = () => {
+    return good + neutral + bad
   }
-  feddbackBtn = (type) => {
-    this.setState((prevState) => ({
-      [type]: prevState[type] + 1
-    }));
-  };
-  handleTotalFeddback = ({ good, neutral, bad }) => {
-    return good + neutral + bad;
-  };
-  handlePositiveFeddback = ({ good, neutral, bad }) => {
+  const handlePositiveFeddback = () => {
   return Math.round((good / (good + neutral + bad)) * 100);
 };
   
-  
-render() {
-  const { good, neutral, bad } = this.state
-  const totalFeddback = this.handleTotalFeddback(this.state)
-  const positiveFeddback = this.handlePositiveFeddback({ good, neutral, bad });
   return (
-      <>
+    
+    
+    <>
+      
     <Section title="Please leave your feedback:">
           <Feddback  
           options={['good', 'neutral', 'bad']}
-          feddbackBtns={this.feddbackBtn}
+          
+          feddbackBtns={handleChange}
           
           />
       </Section >
       <Section title="Statistics:">
-        { totalFeddback?(<Statistics
+        { handleTotalFeddback() ?(<Statistics
           good={good}
           neutral={neutral}
           bad={bad}
-          total={totalFeddback}
-          positivePercentage={positiveFeddback}
+          total={handleTotalFeddback()}
+          positivePercentage={handlePositiveFeddback()}
           />) : (<Message
           message="There is no feedback yet."
-          > </Message>)}
-       </Section>
+          > </Message>)} 
+        </Section>
       </>
-  );}
-};
+  )
+}
+
+
--- src/components/Feddback/Feddback.jsx
+++ src/components/Feddback/Feddback.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 import styles from './Feddback.module.css'
+
 export default function Feddback({ options, feddbackBtns }) {
     return (
       
     options.map(el => (
       <ul className={styles.button_list} key={el}>
-        <button className={styles.buttons} type="button" onClick={() => feddbackBtns(el)}>{el}</button>
+        <button className={styles.buttons}  type="button" onClick={() => feddbackBtns(el)}>{el}</button>
       </ul>
     ))
   );
